5946b88a182c96f5f502c70714297f29ce4ce453
Yeray Boix <al376154@uji.es>
2022-12-16 16:22:44 +0100

    feat(it01): react fronted created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+ 
+function App() {
+ 
+  return (
+    <Router>
+      <div>
+        <section>                              
+            <Routes>
+            </Routes>                    
+        </section>
+      </div>
+    </Router>
+  );
+}
+export default App;
