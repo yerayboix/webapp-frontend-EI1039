62f8b7c51c210caca957eecbf54d813a091dbe42
Pablo Gozalbo Solsona <al394763@uji.es>
2022-12-27 16:26:10 +0100

    feat(it01r01h05): profile WIP

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../config/firebase';
+import { useState } from "react";
 
  
 const Profile = () => {
 
     const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [userServices, setUserServices] = useState('');
+
 
     const user = auth.currentUser.email;
+    const userUID = auth.currentUser.uid;
 
+    const entries = {
+        userUID,
+    };
+
+    fetch("/user",{
+        method:'GET',
+        body: JSON.stringify(entries),
+        headers:{
+            'Content-type':'application/json'
+        },
+    }).then(res=>res.json()).catch(error => console.error('Error:',error))
+    .then(response=>{
+        if(Object.keys(response.mssg).length===3){
+            setUserServices(response.mssg);
+        }
+        else{
+            setErrorMessage(response.mssg);
+            navigate('/');
+        }
+    });
 
     const handleLogout = () => {
 
@@ -28,6 +53,17 @@ const Profile = () => {
                     Welcome to the beta profile, {user} <br></br>
                     Here we can logout, eliminate your account and change your password
                 </p>
+
+                <br></br>
+
+                <div>
+                    <p> 
+                        Here are your services: <br></br>
+                        Weather: {userServices[0]} <br></br>
+                        Events: {userServices[1]} <br></br>
+                        ?¿: {userServices[2]} <br></br>
+                    </p>
+                </div>
  
                 <div>
                     <button onClick={handleLogout}>
