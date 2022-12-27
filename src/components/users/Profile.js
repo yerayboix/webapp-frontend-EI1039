e7e32e7730b50c0e5bdd402b0ae544bc15e8c5e6
Yeray Boix <al376154@uji.es>
2022-12-27 18:01:11 +0100

    feat(it01r01h05): basic profile frontend done

--- src/components/users/Profile.js
+++ src/components/users/Profile.js
@@ -2,38 +2,42 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../config/firebase';
 import { useState } from "react";
+import { useEffect } from 'react';
 
  
 const Profile = () => {
 
     const navigate = useNavigate();
+    const [user, setUser] = useState(auth.currentUser.email);
+    const [userUID, setUserUID] = useState(auth.currentUser.uid);
     const [errorMessage, setErrorMessage] = useState('');
-    const [userServices, setUserServices] = useState('');
+    const [userServices, setUserServices] = useState(null);
 
-
-    const user = auth.currentUser.email;
-    const userUID = auth.currentUser.uid;
-
-    const entries = {
-        userUID,
-    };
-
-    fetch("/user",{
-        method:'GET',
-        body: JSON.stringify(entries),
-        headers:{
-            'Content-type':'application/json'
-        },
-    }).then(res=>res.json()).catch(error => console.error('Error:',error))
-    .then(response=>{
-        if(Object.keys(response.mssg).length===3){
-            setUserServices(response.mssg);
-        }
-        else{
-            setErrorMessage(response.mssg);
-            navigate('/');
-        }
-    });
+    useEffect(() => {
+        let entries = {
+            userUID,
+        };
+        fetch("/user",{
+            method:'POST',
+            body: JSON.stringify(entries),
+            headers:{
+                'Content-type':'application/json'
+            },
+        }).then(res=>res.json())
+        .catch(error => 
+            console.error('Error:',error)
+            )
+        .then((response)=>{
+            console.log(response);
+            if(response.servicesByDefault.length === 3){
+                setUserServices(response.servicesByDefault);
+            }
+            else{
+                setErrorMessage(response.mssg);
+                navigate('/');
+            }
+        });
+    }, [])
 
     const handleLogout = () => {
 
@@ -59,9 +63,9 @@ const Profile = () => {
                 <div>
                     <p> 
                         Here are your services: <br></br>
-                        Weather: {userServices[0]} <br></br>
-                        Events: {userServices[1]} <br></br>
-                        ?¿: {userServices[2]} <br></br>
+                        Weather: {userServices && (userServices[0].toString())} <br></br>
+                        Events: {userServices && (userServices[1].toString())} <br></br>
+                        News: {userServices && (userServices[2].toString())} <br></br>
                     </p>
                 </div>
  
