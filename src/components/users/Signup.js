53ce18a32f3a255dcb5fb5b7ebf0575da7d734db
Marc Navarro Gaya <al386145@uji.es>
2022-12-30 18:14:42 +0100

    signup style implementation

--- src/components/users/Signup.js
+++ src/components/users/Signup.js
@@ -1,6 +1,17 @@
 import React, {useState} from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBIcon,
+    MDBRow,
+    MDBCol,
+    MDBCheckbox
+  }
+  from 'mdb-react-ui-kit';
 
 const Signup = () => {
     const navigate = useNavigate();
@@ -36,61 +47,44 @@ const Signup = () => {
     }
  
   return (
-    <main >        
-        <section>
-            <div>
-                <div>                  
-                    <h1> Register </h1>                                                                            
-                    <form>                                                                                            
-                        <div>
-                            <label htmlFor="email-address">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                label="Email address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}  
-                                required                                    
-                                placeholder="Email address"                                
-                            />
-                        </div>
+    <section className="vh-100" style={{backgroundColor: '#9A616D'}}>
+    <MDBContainer fluid>
 
-                        <div>
-                            <label htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                label="Create password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)} 
-                                required                                 
-                                placeholder="Password"              
-                            />
-                        </div>                                             
-                        
-                        <button
-                            type="submit" 
-                            onClick={onSubmit}                        
-                        >  
-                            Sign up                                
-                        </button>
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
 
-                        {errorMessage && <div className="error"> {errorMessage} </div>}                                     
-                    </form>
-                   
-                    <p>
+          <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
+            <MDBCardBody className='p-5 w-100 d-flex flex-column'>
+
+              <h2 className="h1 fw-bold mb-0" style={{letterSpacing: '1px'}}>Sign up</h2>
+              <p className="text-grey-50 mb-3">Please enter your login and password!</p>
+
+              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' onChange={(e) => setEmail(e.target.value)}  
+                                required value={email} size="lg"/>
+              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' onChange={(e) => setPassword(e.target.value)}
+                                required value={password} size="lg"/>
+
+              <MDBBtn className="btn btn-dark btn-lg btn-block" size='lg' onClick={onSubmit}>
+                Sign up
+              </MDBBtn>
+
+              {errorMessage && <div className="error"> {errorMessage} </div>} 
+              <hr className="my-4" />
+              <p>
                         Already have an account?{' '}
                         <NavLink to="/login" >
-                            Sign in
+                            Log in
                         </NavLink>
-                    </p>                   
-                </div>
-            </div>
-        </section>
-    </main>
-  )
+                    </p>    
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+    </section>
+  );
 }
- 
+
 export default Signup
