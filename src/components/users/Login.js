5b4a8bb038ef9ebd1c1b3c3c5072bc8d64d88c02
Marc Navarro Gaya <al386145@uji.es>
2022-12-30 17:32:32 +0100

    login style

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -28,58 +28,62 @@ const Login = () => {
  
     return(
         <>
-            <main >        
-                <section>
-                    <div>                                            
-                        <p> Login </p>                       
-                                                       
-                        <form>                                              
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"                                    
-                                    required                                                                                
-                                    placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
-                                />
-                            </div>
+            <main >
+            <section className="vh-100" style={{backgroundColor: '#9A616D'}}>
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col col-xl-10">
+                            <div className="card" style={{borderRadius: '1rem'}}>
+                            <div className="row g-0">
+                                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                                    alt="login form" className="img-fluid" style={{borderRadius: '1rem 0 0 1rem'}} />
+                                </div>
+                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div className="card-body p-4 p-lg-5 text-black">
 
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"                                    
-                                    required                                                                                
-                                    placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                />
+                                    <form>
+
+                                    <div className="d-flex align-items-center mb-3 pb-1">
+                                        <i className="fas fa-cubes fa-2x me-3" style={{color: '#ff6219'}}></i>
+                                        <span className="h1 fw-bold mb-0">Login</span>
+                                    </div>
+
+                                    <h5 className="fw-normal mb-3 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
+
+                                    <div className="form-outline mb-4">
+                                        <input type="email" id="email-address" className="form-control form-control-lg" 
+                                                        onChange={(e)=>setEmail(e.target.value)}/>
+                                        <label className="form-label" htmlFor="email-address">Email address</label>
+                                    </div>
+
+                                    <div className="form-outline mb-4">
+                                        <input type="password" id="password" name="password" required className="form-control form-control-lg"
+                                                        onChange={(e)=>setPassword(e.target.value)} />
+                                        <label className="form-label" htmlFor="password">Password</label>
+                                    </div>
+
+                                    <div className="pt-1 mb-4">
+                                        <button className="btn btn-dark btn-lg btn-block" type="button" onClick={onLogin} >Login</button>
+                                    </div>
+                                    
+                                    <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>Don't have an account? {' '}
+                                        <NavLink to="/signup">
+                                            Sign up
+                                        </NavLink>
+                                    </p>
+                                    </form>
+
+                                </div>
+                                </div>
                             </div>
-                                                
-                            <div>
-                                <button                                    
-                                    onClick={onLogin}                                        
-                                >      
-                                    Login                                                                  
-                                </button>
-                            </div>                               
-                        </form>
-                       
-                        <p className="text-sm text-white text-center">
-                            No account yet? {' '}
-                            <NavLink to="/signup">
-                                Sign up
-                            </NavLink>
-                        </p>
-                                                   
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </section>
+                
+
             </main>
         </>
     )
