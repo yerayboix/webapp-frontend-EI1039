e86543fca2ceaa9b651b529434fcb24e8bd697c9
Yeray Boix <al376154@uji.es>
2023-01-03 21:29:49 +0100

    feat(): login and signup translation

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -75,13 +75,13 @@ const Login = () => {
           <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
 
-              <h2 className="h1 fw-bold mb-0" style={{letterSpacing: '1px'}}>Log in</h2>
-              <p className="text-grey-50 mb-3">Please enter your login and password!</p>
+              <h2 className="h1 fw-bold mb-0" style={{letterSpacing: '1px'}}>Iniciar sesión</h2>
+              <p className="text-grey-50 mb-3">Por favor introduce tu correo y contraseña!</p>
 
               <form onSubmit={onLogin}>
-                <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='email' type='email' onChange={(e) => setEmail(e.target.value)}  
+                <MDBInput wrapperClass='mb-4 w-100' label='Email' id='email' type='email' onChange={(e) => setEmail(e.target.value)}  
                                   required value={email} size="lg"/>
-                <MDBInput wrapperClass='mb-4 w-100' label='Password' id='password' type='password' onChange={(e) => setPassword(e.target.value)}
+                <MDBInput wrapperClass='mb-4 w-100' label='Contraseña' id='password' type='password' onChange={(e) => setPassword(e.target.value)}
                                   required value={password} size="lg"/>
 
                 <MDBBtn type='submit' className="btn btn-dark btn-lg btn-block" size='lg' >
@@ -91,9 +91,9 @@ const Login = () => {
 
               {errorMessage && <div className="error"> {errorMessage} </div>} 
               <hr className="my-4" />
-              <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>Don't have an account? {' '}
+              <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>No tienes una cuenta? {' '}
                                         <NavLink to="/signup">
-                                            Sign up
+                                            Registrate
                                         </NavLink>
                                     </p>   
             </MDBCardBody>
--- src/components/users/Signup.js
+++ src/components/users/Signup.js
@@ -54,24 +54,24 @@ const Signup = () => {
           <MDBCard className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
 
-              <h2 className="h1 fw-bold mb-0" style={{letterSpacing: '1px'}}>Sign up</h2>
-              <p className="text-grey-50 mb-3">Please enter your login and password!</p>
+              <h2 className="h1 fw-bold mb-0" style={{letterSpacing: '1px'}}>Registrarse</h2>
+              <p className="text-grey-50 mb-3">Por favor introduce tu correo y contraseña!</p>
 
-              <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='email' type='email' onChange={(e) => setEmail(e.target.value)}  
+              <MDBInput wrapperClass='mb-4 w-100' label='Email' id='email' type='email' onChange={(e) => setEmail(e.target.value)}  
                                 required value={email} size="lg"/>
-              <MDBInput wrapperClass='mb-4 w-100' label='Password' id='password' type='password' onChange={(e) => setPassword(e.target.value)}
+              <MDBInput wrapperClass='mb-4 w-100' label='Contraseña' id='password' type='password' onChange={(e) => setPassword(e.target.value)}
                                 required value={password} size="lg"/>
 
               <MDBBtn type='submit' onClick={onSubmit} className="btn btn-dark btn-lg btn-block" size='lg' >
-                Sign up
+              Registrate!
               </MDBBtn>
 
               {errorMessage && <div className="error"> {errorMessage} </div>} 
               <hr className="my-4" />
               <p>
-                  Already have an account?{' '}
+                  Ya tienes una cuenta?{' '}
                   <NavLink to="/login" >
-                      Log in
+                      Inicia sesión
                   </NavLink>
               </p>    
             </MDBCardBody>
